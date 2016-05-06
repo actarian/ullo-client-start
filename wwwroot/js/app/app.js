35b97efbee41b967cd3f1a84631c3a5cea15f205
actarian <lzampetti@gmail.com>
2016-05-06 18:59:17 +0200

    Lesson 2

--- wwwroot/js/app/app.js
+++ wwwroot/js/app/app.js
@@ -11,3 +11,58 @@ var CONFIG = {
     },
     IOS: (navigator.userAgent.match(/iPad|iPhone|iPod/g) ? true : false),
 };
+
+var app = angular.module('ullo', []);
+
+app.controller('TestCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+
+    $scope.model = {
+        title: 'Titolo',
+    };
+    
+    $timeout(function() {
+        $scope.model.title = 'Titolone';
+    }, 1000);
+
+    $scope.item = {
+        id: 117,
+        user: {
+            userName: 'Fabio Ottaviani',
+            facebookId: '10153341954226947',
+            route: 'fabio-ottaviani'
+        },
+        dish: {
+            price: 5,
+            isVeganFriendly: false,
+            yes: 1,
+            no: 0,
+            created: '2016-04-27T19:13:45.497',
+            vote: {
+                dishId: 19,
+                like: true,
+                created: '2016-04-27T19:14:00.497'
+            },
+            categories: [
+                {
+                    id: 2,
+                    name: 'Piadine',
+                    key: 'piadine'
+                }
+            ],
+            id: 19,
+            name: 'Pizza Margherita',
+            key: 'pizzaMargherita'
+        },
+        picture: {
+            guid: '8fe99743-4ed3-46de-ba13-2c1bd7a45ffe',
+            created: '2016-04-27T19:13:41.02',
+            id: 20,
+            name: '8fe99743-4ed3-46de-ba13-2c1bd7a45ffe',
+            route: '/Media/Files/8fe99743-4ed3-46de-ba13-2c1bd7a45ffe.jpg',
+            key: '8Fe997434Ed346DeBa132C1bd7a45ffe'
+        },
+        created: '2016-04-27T19:13:45.497'
+    };
+
+}]);
+
